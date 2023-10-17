a0f3622dc5c2257f699f8c7abdb018926f2d9303
Shakhov Pavel <pshakhov@fil-it.ru>
2023-10-17 21:25:55 +0300

    todo list component added

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoListService } from "../services/todo-list.service";
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+
+  constructor(
+    public todoListService: TodoListService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  listTitle:string = "TODO's :"
+
+  protected readonly TodoListService = TodoListService;
+}
