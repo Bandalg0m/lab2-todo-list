348208198925fe73ee6e721c365e8b8766dab698
Shakhov Pavel <pshakhov@fil-it.ru>
2023-10-23 14:57:26 +0300

    Auth service update

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,6 +1,5 @@
-import {Inject, Injectable} from '@angular/core';
-import {API_URL} from "../environment/environment.service";
-import {IEnvironmentInterface} from "../../../environments/IEnvironmentInterface";
+import { Injectable } from '@angular/core';
+import { EnvironmentService } from "../environment/environment.service";
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
 import {map} from "rxjs";
@@ -13,12 +12,12 @@ export class AuthService {
 
   baseUrl: string;
 
-  constructor(@Inject(API_URL) environment: IEnvironmentInterface, private http: HttpClient, private router: Router) {
-    this.baseUrl = environment.apiUrl
+  constructor(private environment: EnvironmentService, private http: HttpClient, private router: Router) {
+    this.baseUrl = environment.getApiUrl()
   }
 
   public login(email: string, password: string) {
-    return this.http.post<{ token: string }>(`${this.baseUrl}/login`, {email, password}).pipe(map((res) => {
+    return this.http.post<{ token: string }>(`${this.baseUrl}/auth/login`, {email, password}).pipe(map((res) => {
       res.token ? localStorage.setItem('auth-token', res.token) : null;
       (localStorage.getItem('auth-token') !== null) ? this.router.navigate(['main']) : null;
     }))
